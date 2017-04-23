af74b3310df8898e208aede3665ede82393099ac
Rohit Gattani <rohitagattani@gmail.com>
2017-04-22 23:29:52 -0400

    stable baseline

--- sentiment.py
+++ sentiment.py
@@ -1,149 +1,174 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import re
 import numpy as np
 import pandas as pd
-import nltk
-from nltk.corpus import stopwords
-from sklearn.model_selection import train_test_split
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from time import time
 
-stopWords = stopwords.words('english')
+from sklearn.model_selection import train_test_split
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
 
 # read csv data file and keep only required columns
-data = pd.read_csv('input/Sentiment.csv')
+print('[INFO]: loading data...')
+data = pd.read_csv('input/US_Airline.csv')
 data = data[['text', 'sentiment']]
+print('[INFO]: data loaded')
 
-# split the dataset into train and test set
-trainData, testData = train_test_split(data, test_size = 0.2)
+# split the dataset into a training set and a test set
+trainingData, testData = train_test_split(data, test_size = 0.2, random_state=42)
+
+# store tweets and labels in separate variables
+trainingTweets = trainingData['text']
+testTweets = testData['text']
+trainingLabels = trainingData['sentiment']
+testLabels = testData['sentiment']
+
+# display training and test data size
+def sizeMB(docs):
+	return sum(len(s.encode('utf-8')) for s in docs) / 1e6
+
+print('[INFO]: (training set) %d tweets - %0.3fMB' %(len(trainingTweets), sizeMB(trainingTweets)))
+print('[INFO]:     (test set) %d tweets - %0.3fMB' %(len(testTweets), sizeMB(testTweets)))
+print()
+
+# display categorical details of dataset 
+def printGroupByDetails(data, groupByColumn):
+	groupedData = data.groupby(by=groupByColumn)
+	for name, group in groupedData:
+		print(" %s => %d " %(name, len(group)))
+
+print('[INFO]: training set details')
+printGroupByDetails(trainingData, 'sentiment')
 
 
-# ****************************************************************
 # preprocessing
-regexRules = [(r'(?<=^|(?<=[^a-zA-Z0-9-_\\.]))@[A-Za-z]+[A-Za-z0-9_]+', ''),
+tokenRegexRules = [(r'(?<=^|(?<=[^a-zA-Z0-9-_\\.]))@[A-Za-z]+[A-Za-z0-9_]+', ''),
 			  (r'(?<=^|(?<=[^a-zA-Z0-9-_\\.]))#[A-Za-z]+[A-Za-z0-9_]+', ''),
 			  (r'(http|https|ftp)\S+', ''),
 			  (r'((\w)\2{2,})', r'\2')]
 
-emoticons = [	
-		('__EMOT_SMILEY',	[':-)', ':)', '(:', '(-:', ] )	,
-		('__EMOT_LAUGH',		[':-D', ':D', 'X-D', 'XD', 'xD', ] )	,
-		('__EMOT_LOVE',		['<3', ':\*', ] )	,
-		('__EMOT_WINK',		[';-)', ';)', ';-D', ';D', '(;', '(-;', ] )	,
-		('__EMOT_FROWN',		[':-(', ':(', '(:', '(-:', ] )	,
-		('__EMOT_CRY',		[':,(', ':\'(', ':"(', ':(('] )	,
-	]
+emoticonsDictionary = [	
+		([':-)', ':)', '(:', '(-:',], 'EMOT_SMILEY'),
+		([':-D', ':D', 'X-D', 'XD', 'xD',], 'EMOT_LAUGH'),
+		(['<3', ':\*', ], 'EMOT_LOVE',		),
+		([';-)', ';)', ';-D', ';D', '(;', '(-;',], 'EMOT_WINK'),
+		([':-(', ':(', '(:', '(-:',], 'EMOT_FROWN'),
+		([':,(', ':\'(', ':"(', ':((',], 'EMOT_CRY')]
 
-#For emoticon regexes
-def escape_paren(arr):
-	return [text.replace(')', '[)}\]]').replace('(', '[({\[]') for text in arr]
+# for emoticon regexes
+def buildEmoticonRegex(emoticons):
+	emoticons = [emoticon.replace(')', '[)}\]]').replace('(', '[({\[]') for emoticon in emoticons]
+	emoticons = '(' + '|'.join(emoticons) + ')'
+	return re.compile(emoticons)	
 
-def regex_union(arr):
-	return '(' + '|'.join( arr ) + ')'
+emoticonsRegexRules = [ (buildEmoticonRegex(emoticons), replace) for (emoticons, replace) in emoticonsDictionary]
 
-emoticons_regex = [ (re.compile(regex_union(escape_paren(regx))), repl) for (repl, regx) in emoticons ]
-
-# remove user-mentions, hashtags, links and replace repeated characters (>2)
-def preprocessData(data, regexRules):
+# remove user-mentions, hashtags, links and replace repeated characters, emoticons (>2)
+def preprocessData(data):
 	tweets = []
 	for (index, row) in data.iterrows():
 		text = row.text.lower()
-		for (pattern, replace) in regexRules:
+		for (pattern, replace) in tokenRegexRules:
 			text = re.sub(pattern, replace, text)
-		for (pattern, replace) in emoticons_regex:
+		for (pattern, replace) in emoticonsRegexRules:
 			text = re.sub(pattern, replace, text)
 		tweets.append(text)
 	return tweets
 
-trainingTweets = preprocessData(trainData, regexRules)
-testingTweets = preprocessData(testData, regexRules)
+processedTrainingTweets = preprocessData(trainingData)
+processedTestTweets = preprocessData(testData)
+
+# feature extraction
+print('[INFO]: extracting features from the training data...')
+vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,2), sublinear_tf=True, max_df=0.5)
+trainingDtm = vectorizer.fit_transform(processedTrainingTweets)
+
+print('[INFO]: extracting features from the test data...')
+testDtm = vectorizer.transform(processedTestTweets)
 
 
-# ****************************************************************
-## training a classifier (using a pipeline)
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn import metrics
+# benchmarking
+def benchmark(classifier, classifierName):
+	# train the classifier and predict class-labels
+	print('[INFO]: Training the classifier on the training set')
+	startTime = time()
+	classifier.fit(trainingDtm, trainingLabels)
+	trainTime = time() - startTime
+	print('[INFO]: Training took %0.3fs' %(trainTime))
 
+	print('[INFO]: Predicting the outcome of the test set')
+	startTime = time()
+	predictedLabels = classifier.predict(testDtm)
+	testTime = time() - startTime
+	print('[INFO]: Prediction took %0.3fs' %(testTime))
 
-# Naive Bayes
-print('Naive Bayes')
-from sklearn.naive_bayes import MultinomialNB
-text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
-					('tfidf', TfidfTransformer()),
-					('clf', MultinomialNB())
-			])
-_ = text_clf.fit(trainingTweets, trainData['sentiment'])
-predicted = text_clf.predict(testingTweets)
-#for tweet, sentimentCategory in zip(testData['text'], predicted):
-#	print('%s => %s' %(tweet, sentimentCategory))
-print('MEAN: ', np.mean(predicted == testData['sentiment']))
+	# calculate accuracy score, display report and build confusion matrix
+	score = metrics.accuracy_score(testLabels, predictedLabels)
+	print('[INFO]: Accuracy: %0.3f' % score)
 
-print(metrics.classification_report(testData['sentiment'], predicted))
-print(metrics.confusion_matrix(testData['sentiment'], predicted))
+	print('[INFO]: Classification report:')
+	print(metrics.classification_report(testLabels, predictedLabels))
 
+	confusionMatrix = metrics.confusion_matrix(testLabels, predictedLabels)
+	print('[INFO]: Confusion matrix:')
+	print(confusionMatrix)
 
-## parameters tuining using grid search
-from sklearn.model_selection import GridSearchCV
-parameters = {	
-				'vect__ngram_range':[(1, 1), (1, 2)],
-				'tfidf__use_idf':(True, False),
-				'clf__alpha':(1e-2, 1e-3)
-			}
-gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
-gs_clf = gs_clf.fit(trainingTweets, trainData['sentiment'])
-print('Best score: ', gs_clf.best_score_)
+	"""
+	# plot confusion matrix
+	plt.matshow(confusionMatrix)
+	plt.title('Confusion matrix (%s)' % classifierName)
+	plt.colorbar()
+	"""
 
-for param_name in sorted(parameters.keys()):
-	print("%s: %r" %(param_name, gs_clf.best_params_[param_name]))
+	# return benchmark parameters
+	return classifierName, score, trainTime, testTime
 
+# selected classifiers for testing
+classifiers = [
+	        (KNeighborsClassifier(n_neighbors=10), "K Nearest Neighbors"),
+	        (MultinomialNB(alpha=1e-2), "Multinomial Naive Bayes"),
+	        (LinearSVC(penalty='l2', dual=False, tol=1e-4), "Linear SVC"),
+	        (SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=50), "SGD Classifier-l2"),
+	        (SGDClassifier(loss='hinge', penalty='elasticnet', alpha=1e-3, n_iter=50), "SGD Classifier-elasticnet"),
+	        (MLPClassifier(alpha=1e-2), "Multi-layer Perceptron")]
 
-# ****************************************************************
-# svm
-print('svm')
-from sklearn.linear_model import SGDClassifier
-text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
-					('tfidf', TfidfTransformer()),
-					('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=10))
-			])
-_ = text_clf.fit(trainingTweets, trainData['sentiment'])
-predicted = text_clf.predict(testingTweets)
-#for tweet, sentimentCategory in zip(testData['text'], predicted):
-#	print('%s => %s' %(tweet, sentimentCategory))
-print('MEAN: ', np.mean(predicted == testData['sentiment']))
+# benchmark classifiers and store results
+results = []
+for classifier, classifierName in classifiers:
+	print('=' * 80)
+	print(classifierName)
+	results.append(benchmark(classifier, classifierName))
 
-print(metrics.classification_report(testData['sentiment'], predicted))
-print(metrics.confusion_matrix(testData['sentiment'], predicted))
+# make some plots
+indices = np.arange(len(results))
+results = [[result[idx] for result in results] for idx in range(4)]
+classifierNames, score, trainingTime, testTime = results
 
+trainingTime = np.array(trainingTime) / np.max(trainingTime)
+testTime = np.array(testTime) / np.max(testTime)
 
-## parameters tuining using grid search
-from sklearn.model_selection import GridSearchCV
-parameters = {	
-				'vect__ngram_range':[(1, 1), (1, 2)],
-				'tfidf__use_idf':(True, False),
-				'clf__alpha':(1e-2, 1e-3)
-			}
-gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
-gs_clf = gs_clf.fit(trainingTweets, trainData['sentiment'])
-print('Best score: ', gs_clf.best_score_)
+plt.figure(figsize=(12, 8))
+plt.title("Score")
+plt.barh(indices, score, .2, label="score", color='navy')
+plt.barh(indices + .3, trainingTime, .2, label="training time", color='c')
+plt.barh(indices + .6, testTime, .2, label="test time", color='darkorange')
+plt.yticks(())
+plt.legend(loc='best')
+plt.subplots_adjust(left=.25)
+plt.subplots_adjust(top=.95)
+plt.subplots_adjust(bottom=.05)
 
-for param_name in sorted(parameters.keys()):
-	print("%s: %r" %(param_name, gs_clf.best_params_[param_name]))
+for idx, classifierName in zip(indices, classifierNames):
+    plt.text(-.3, idx, classifierName)
 
-
-# ****************************************************************
-# Multi-layer perceptron
-print('MLP')
-from sklearn.neural_network import MLPClassifier
-text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
-					('tfidf', TfidfTransformer()),
-					('clf', MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1))
-			])
-_ = text_clf.fit(trainingTweets, trainData['sentiment'])
-predicted = text_clf.predict(testingTweets)
-#for tweet, sentimentCategory in zip(testData['text'], predicted):
-#	print('%s => %s' %(tweet, sentimentCategory))
-print('MEAN: ', np.mean(predicted == testData['sentiment']))
-
-print(metrics.classification_report(testData['sentiment'], predicted))
-print(metrics.confusion_matrix(testData['sentiment'], predicted))
+plt.show()
